b90da7460732a8e614501fe93984585628c47128
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-04-26 22:12:35 +0300

    mobile wiki new ver

--- src/test/java/tests/FirstMobileTest.java
+++ src/test/java/tests/FirstMobileTest.java
@@ -16,8 +16,7 @@ public class FirstMobileTest extends TestBase {
     void searchNewWikiTest() {
 
         step("Skip Language", () -> {
-           // $("By.AccessibilityId: Skip").click();});
-            $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();});
+               $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();});
         step("Type search", () -> {
                  $(MobileBy.AccessibilityId("Search Wikipedia")).click();
                  $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("Selenide"); });
