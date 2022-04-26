45d057401e1b64a789fc09aeffe1df174f7f8568
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-04-26 21:55:10 +0300

    mobile wiki new ver

--- src/test/java/tests/FirstMobileTest.java
+++ src/test/java/tests/FirstMobileTest.java
@@ -15,10 +15,8 @@ public class FirstMobileTest extends TestBase {
     @Test
     void searchNewWikiTest() {
 
-        step("Skip Language", () ->
-                $((MobileBy.AccessibilityId("Skip"))
-                        // $(MobileBy.id("org.wikipedia.alpha:id/android.widget.TextViewandroid.widget.TextView")).submit());
-                 $(MobileBy.id("android.widget.FrameLayout:id/fragment_onboarding_skip_button")).submit());
+        step("Skip Language", () -> {
+                 $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();});
         step("Type search", () -> {
                  $(MobileBy.AccessibilityId("Search Wikipedia")).click();
                  $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("Selenide"); });
